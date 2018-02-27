2b4732610129eebeb9303b76afc70a87c6cf261a
ShootMe <birreller@gmail.com>
2018-02-27 15:30:28 -0700

    Update studio

--- Tools/Studio.cs
+++ Tools/Studio.cs
@@ -132,11 +132,16 @@ namespace CelesteStudio {
 					}
 					if (lastChanged.AddSeconds(0.6) < DateTime.Now) {
 						lastChanged = DateTime.Now;
+						bool shouldSave = false;
 						this.Invoke((Action)delegate () {
 							if ((!string.IsNullOrEmpty(tasText.LastFileName) || !string.IsNullOrEmpty(tasText.SaveToFileName)) && tasText.IsChanged) {
-								tasText.SaveFile();
+								shouldSave = true;
 							}
 						});
+
+						if (shouldSave) {
+							tasText.SaveFile();
+						}
 					}
 					if (hooked) {
 						UpdateValues();
