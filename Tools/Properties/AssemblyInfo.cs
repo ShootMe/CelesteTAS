5e95e9e3cf864e3f5abda4cc1cb8657d47594e6b
ShootMe <birreller@gmail.com>
2018-03-30 15:24:22 -0600

    Update

--- Tools/Properties/AssemblyInfo.cs
+++ Tools/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("51388820-c0ee-420a-bb43-94c7f75145fc")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
