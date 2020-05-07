aa8d52727f22ce30d8f979a9b7ba8768f21624e0
PenguinOwl <henryjboles@gmail.com>
2020-05-07 07:00:11 -0700

    Add linux support to Celeste Studio

---
+++ Tools/RichText/NativeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CelesteStudio
+{
+    internal class NativeMethods
+    {
+        [DllImport("Imm32.dll")]
+        internal static extern IntPtr ImmGetContext(IntPtr hWnd);
+
+        [DllImport("Imm32.dll")]
+        internal static extern IntPtr ImmAssociateContext(IntPtr hWnd, IntPtr hIMC);
+
+        [DllImport("User32.dll")]
+        internal static extern bool CreateCaret(IntPtr hWnd, int hBitmap, int nWidth, int nHeight);
+
+        [DllImport("User32.dll")]
+        internal static extern bool SetCaretPos(int x, int y);
+
+        [DllImport("User32.dll")]
+        internal static extern bool DestroyCaret();
+
+        [DllImport("User32.dll")]
+        internal static extern bool ShowCaret(IntPtr hWnd);
+
+        [DllImport("User32.dll")]
+        internal static extern bool HideCaret(IntPtr hWnd);
+    }
+}
